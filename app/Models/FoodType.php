b1551b5ef06b425c3eca71822c92124a43455c6e
newtonjob <jobnewton3@gmail.com>
2023-04-24 17:34:04 +0100

    Create models for `food` and `foodtype`

---
+++ app/Models/FoodType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FoodType extends Model
+{
+    /**
+     * The table associated with the model.
+     */
+    protected $table = 'foodtype';
+
+    /**
+     * The attributes that aren't mass assignable.
+     */
+    protected $guarded = [];
+}
